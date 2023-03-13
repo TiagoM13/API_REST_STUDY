25641a003ec8958e7b20abb692075fb95e4eb761
TiagoM13 <thyagomotha2000@gmail.com>
2023-03-12 22:45:42 -0300

    create server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+require("./controllers/authController")(app);
+
+app.listen(3000, () => {
+  console.log("Server running...");
+});
